59449cdf373e0e9de89081090148080b730c3873
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2024-11-15 09:32:23 +0500

    remove token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,21 @@
 import { jwtDecode } from 'jwt-decode';
 
 const handleToken = {
-    getToken: () => localStorage.getItem("token") || null,
+    getToken: () => {
+        const token = localStorage.getItem("token") || null
+        try {
+            const decodedToken: { exp: number } = jwtDecode(token ?? '');
+            const currentTime = Date.now() / 1000;
+            if (decodedToken.exp < currentTime) {
+                localStorage.removeItem("token");
+                return null;
+            }
+            return token;
+        } catch (error) {
+            console.error("Ошибка декодирования токена:", error);
+            localStorage.removeItem("token");
+        }
+    },
     removeToken: () => {
         localStorage.removeItem("token")
         window.location.reload();
@@ -13,7 +27,7 @@ export default handleToken
 
 export function getTokenId() {
     try {
-        const token = localStorage.getItem("token");
+        const token = handleToken.getToken();
         if (token) {
             let user: {
                 id: number
